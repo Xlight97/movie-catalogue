6d519309648fad4ce95449f7e392335eb861a4db
Xlight97 <xlight97@gmail.com>
2023-07-25 11:31:01 +0800

    Change karma configuration in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -58,11 +58,11 @@ module.exports = (config) => {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // browsers: ['C:/Program Files/Google/Chrome/Application/chrome.exe'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
